df21bbba60e39267e5c178007d5da8a6c475c4ec
mbhat06 <bhat.manjunath@gmail.com>
2014-09-19 02:19:49 +0530

    change the stupid port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(5000);
+}).listen(3000);
 
 
 // var express = require('express');
