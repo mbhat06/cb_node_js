bccce9d6e36704360eb2cdc4e52c886179090bea
mbhat06 <bhat.manjunath@gmail.com>
2014-09-19 03:36:54 +0530

    again the port

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var static = require('node-static');
 var http = require('http');
-
 var file = new(static.Server)();
 
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(5000);
+}).listen(process.env.PORT || 5000);
 
 
 // var express = require('express');
